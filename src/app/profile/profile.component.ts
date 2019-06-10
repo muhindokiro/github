186c853a0f6652566121c971dfb89f9066c09f0b
muhindokiro <blaize1143@gmail.com>
2019-06-10 16:04:14 +0300

    update file

---
+++ src/app/profile/profile.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from "@angular/core";
+import { User } from "../user";
+import { GithubService } from "../githubsearch.service";
+
+@Component({
+  selector: "app-profile",
+  templateUrl: "./profile.component.html",
+  styleUrls: ["./profile.component.css"],
+  providers: [GithubService]
+})
+
+export class ProfileComponent implements OnInit {
+ 
+  user: User;
+
+  constructor(
+    private githubService: GithubService,
+    
+  ) {
+    this.user = this.githubService.user;
+  }
+  
+  ngOnInit() {
+    this.user = this.githubService.user;
+  }
+}
