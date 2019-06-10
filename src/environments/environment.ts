587d1a3c0e830aecbebfc723ea02524b8fab1bc4
muhindokiro <49603911+muhindokiro@users.noreply.github.com>
2019-06-10 16:55:06 +0300

    add api url for github users

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,5 @@ export const environment = {
   
   production: false,
   apiUrl: "https://api.github.com/users/",
-  apikey: "?access_token=0ff610d62888f906dbf5c067b0c783b468b7462f",
-  
+  apikey: "?access_token=0ff610d62888f906dbf5c067b0c783b468b7462f", 
 };
